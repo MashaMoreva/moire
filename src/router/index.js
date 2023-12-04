1ccd9e1c70e439def085d73bae805b11e2dbfd4d
Masha Moreva <mashamiskova@yandex.ru>
2023-12-04 19:09:14 +0300

    added-route-main

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import MainPage from '@/pages/MainPage.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    name: 'main',
+    component: MainPage,
     path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
 ];
 
