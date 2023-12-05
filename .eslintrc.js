24e6cd35c3bf4de4e9d22fe30fcf5df55a68cd6c
Masha Moreva <mashamiskova@yandex.ru>
2023-12-05 15:13:07 +0300

    added-new-rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vuejs-accessibility/label-has-for': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
