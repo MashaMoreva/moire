ba43ae57ca8fdc0cc6b40fa83e76a55d9f0bd5a1
Masha Moreva <mashamiskova@yandex.ru>
2023-12-05 15:15:44 +0300

    added-app-routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import MainPage from '@/pages/MainPage.vue';
+import ProductPage from '@/pages/ProductPage.vue';
+import CartPage from '@/pages/CartPage.vue';
+import OrderPage from '@/pages/OrderPage.vue';
+import OrderInfoPAge from '@/pages/OrderInfoPage.vue';
+import NotFoundPage from '@/pages/NotFoundPage.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    name: 'main',
-    component: MainPage,
-    path: '/',
-  },
+  { name: 'main', component: MainPage, path: '/' },
+  { name: 'product', component: ProductPage, path: '/product/:id' },
+  { name: 'cart', component: CartPage, path: '/cart' },
+  { name: 'order', component: OrderPage, path: '/orders' },
+  { name: 'orderInfo', component: OrderInfoPAge, path: '/orders/:id' },
+  { name: 'notFound', component: NotFoundPage, path: '*' },
 ];
 
 const router = new VueRouter({
