a756c7fb3ebda8ea28296e8494df3688e19e5aa7
Masha Moreva <mashamiskova@yandex.ru>
2023-12-06 16:18:20 +0300

    created-product-item-component

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="catalog__item">
+    <a class="catalog__pic" href="#">
+      <img :src="image" :alt="product.title" />
+    </a>
+
+    <h3 class="catalog__title">
+      <a href="#"> {{ product.title }} </a>
+    </h3>
+
+    <span class="catalog__price"> {{ product.price }} ₽</span>
+  </li>
+</template>
+<script>
+import noPhoto from '@/assets/img/noPhoto.webp';
+
+export default {
+  props: ['product'],
+  data() {
+    return {
+      image: this.product.colors[0].gallery
+        ? this.product.colors[0].gallery[0].file.url
+        : noPhoto,
+    };
+  },
+};
+</script>
