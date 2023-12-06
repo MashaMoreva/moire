8ad0984afd9ff7e12d92a48c498c2825682ae827
Masha Moreva <mashamiskova@yandex.ru>
2023-12-06 12:41:46 +0300

    add-computed-property-current-year

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,13 +9,17 @@
           <a class="footer__link" href="tel:88006009009"> 8 800 600 90 09 </a>
         </li>
         <li>
-          <a class="footer__link" href="mailto:hi@technozavrrr.com"> hi@technozavrrr.com </a>
+          <a class="footer__link" href="mailto:hi@technozavrrr.com">
+            hi@technozavrrr.com
+          </a>
         </li>
         <li>
           <a class="footer__link" href="#"> Распродажа </a>
         </li>
         <li>
-          <a class="footer__link footer__link--medium" href="#"> Заказать звонок </a>
+          <a class="footer__link footer__link--medium" href="#">
+            Заказать звонок
+          </a>
         </li>
       </ul>
 
@@ -58,29 +62,47 @@
       </ul>
 
       <p class="footer__desc">
-        Все права на&nbsp;материалы, находящиеся на&nbsp;сайте, охраняются в&nbsp;соответствии
-        с&nbsp;законодательством&nbsp;РФ, в&nbsp;том числе об&nbsp;авторском праве и&nbsp;смежных
-        правах.
+        Все права на&nbsp;материалы, находящиеся на&nbsp;сайте, охраняются
+        в&nbsp;соответствии с&nbsp;законодательством&nbsp;РФ, в&nbsp;том числе
+        об&nbsp;авторском праве и&nbsp;смежных правах.
       </p>
 
       <ul class="footer__data">
         <li>
-          <a class="footer__link" href="#" target="_blank" rel="noopener" type="application/pdf">
+          <a
+            class="footer__link"
+            href="#"
+            target="_blank"
+            rel="noopener"
+            type="application/pdf"
+          >
             Политика конфиденциальности
           </a>
         </li>
         <li>
-          <a class="footer__link" href="#" target="_blank" rel="noopener" type="application/pdf">
+          <a
+            class="footer__link"
+            href="#"
+            target="_blank"
+            rel="noopener"
+            type="application/pdf"
+          >
             Публичная оферта
           </a>
         </li>
       </ul>
 
-      <span class="footer__copyright"> © 2020 Moire </span>
+      <span class="footer__copyright">&copy; {{ currentYear }} Moire </span>
     </div>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    },
+  },
+};
 </script>
