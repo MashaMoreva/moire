2b4a0d42d96f0c2f587c6e03b5756345c28ea1ed
Masha Moreva <mashamiskova@yandex.ru>
2023-12-06 18:55:21 +0300

    create-app-paginstion-component

---
+++ src/components/AppPagination.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow pagination__link--disabled"
+        aria-label="Предыдущая страница"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
+      <a
+        href="#"
+        class="pagination__link"
+        :class="{ 'pagination__link--current': pageNumber === page }"
+        @click.prevent="paginate(pageNumber)"
+      >
+        {{ pageNumber }}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow"
+        href="#"
+        aria-label="Следующая страница"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  model: {
+    prop: 'page',
+    event: 'paginate',
+  },
+  props: ['page', 'count', 'perPage'],
+  computed: {
+    pages() {
+      return Math.ceil(this.count / this.perPage);
+    },
+  },
+  methods: {
+    paginate(page) {
+      this.$emit('paginate', page);
+    },
+  },
+};
+</script>
