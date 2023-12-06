eccff31b5ce2808f15b499aaf4be4ae616239b18
Masha Moreva <mashamiskova@yandex.ru>
2023-12-06 16:17:01 +0300

    created-order-info-page-component

---
+++ src/pages/OrderInfoPage.vue
@@ -0,0 +1,87 @@
+<template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html"> Каталог </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="cart.html"> Корзина </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link"> Оформление заказа </a>
+        </li>
+      </ul>
+
+      <h1 class="content__title">Заказ оформлен <span>№ 23621</span></h1>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST">
+        <div class="cart__field">
+          <p class="cart__message">
+            Благодарим за&nbsp;выбор нашего магазина. На&nbsp;Вашу почту придет
+            письмо с&nbsp;деталями заказа. Наши менеджеры свяжутся с&nbsp;Вами
+            в&nbsp;течение часа для уточнения деталей доставки.
+          </p>
+
+          <ul class="dictionary">
+            <li class="dictionary__item">
+              <span class="dictionary__key"> Получатель </span>
+              <span class="dictionary__value">
+                Иванова Василиса Алексеевна
+              </span>
+            </li>
+            <li class="dictionary__item">
+              <span class="dictionary__key"> Адрес доставки </span>
+              <span class="dictionary__value">
+                Москва, ул. Ленина, 21, кв. 33
+              </span>
+            </li>
+            <li class="dictionary__item">
+              <span class="dictionary__key"> Телефон </span>
+              <span class="dictionary__value"> 8 800 989 74 84 </span>
+            </li>
+            <li class="dictionary__item">
+              <span class="dictionary__key"> Email </span>
+              <span class="dictionary__value"> lalala@mail.ru </span>
+            </li>
+            <li class="dictionary__item">
+              <span class="dictionary__key"> Способ оплаты </span>
+              <span class="dictionary__value"> картой при получении </span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="cart__block">
+          <ul class="cart__orders">
+            <li class="cart__order">
+              <h3>Смартфон Xiaomi Redmi Note 7 Pro 6/128GB</h3>
+              <b>990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+            <li class="cart__order">
+              <h3>Гироскутер Razor Hovertrax 2.0ii</h3>
+              <b>1 990 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+            <li class="cart__order">
+              <h3>Электрический дрифт-карт Razor Lil’ Crazy</h3>
+              <b>4 090 ₽</b>
+              <span>Артикул: 150030</span>
+            </li>
+          </ul>
+
+          <div class="cart__total">
+            <p>Доставка: <b>бесплатно</b></p>
+            <p>Итого: <b>3</b> товара на сумму <b>4 070 ₽</b></p>
+          </div>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
