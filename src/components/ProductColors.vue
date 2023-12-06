8fb4fb6023fd5a28ac064c642a33a3f6a8a13a68
Masha Moreva <mashamiskova@yandex.ru>
2023-12-06 16:18:57 +0300

    created-product-colors-component

---
+++ src/components/ProductColors.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="colors colors--black">
+    <li class="colors__item">
+      <label class="colors__label">
+        <input
+          class="colors__radio sr-only"
+          type="radio"
+          name="color-1"
+          value="#73B6EA"
+        />
+        <span class="colors__value" style="background-color: #73b6ea"> </span>
+      </label>
+    </li>
+  </ul>
+</template>
+<script>
+export default {};
+</script>
