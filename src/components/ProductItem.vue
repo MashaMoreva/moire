d2cd37981e3a266406736dbb3ce6b14a595bb111
Masha Moreva <mashamiskova@yandex.ru>
2023-12-07 12:25:24 +0300

    added-color-tracking-for-image-updates

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -9,7 +9,11 @@
     </h3>
 
     <span class="catalog__price"> {{ product.price }} ₽</span>
-    <ProductColors :colors="product.colors" />
+    <ProductColors
+      :colors="product.colors"
+      @colorChanged="updateImage"
+      :currentColor="currentColor"
+    />
   </li>
 </template>
 <script>
@@ -24,7 +28,21 @@ export default {
       image: this.product.colors[0].gallery
         ? this.product.colors[0].gallery[0].file.url
         : noPhoto,
+      currentColor: this.product.colors[0].color.code,
     };
+  },
+  methods: {
+    updateImage(newColorCode) {
+      const selectedColor = this.product.colors.find(
+        (color) => color.color.code === newColorCode,
+      );
+
+      if (selectedColor && selectedColor.gallery.length > 0) {
+        this.image = selectedColor.gallery[0].file.url;
+      } else {
+        this.image = noPhoto;
+      }
+    },
   },
 };
 </script>
