5572a1515a06388dcaad0a6c49fa08c0c1331bd8
Masha Moreva <mashamiskova@yandex.ru>
2023-12-08 16:18:01 +0300

    refactor-removed-hardcoding

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,16 +1,25 @@
 <!-- eslint-disable vuejs-accessibility/form-control-has-label -->
 <template>
-  <main class="content container">
+  <main v-if="loading" class="content container">
+    <CustomLoader />
+  </main>
+  <main v-else class="content container">
     <div class="content__top">
       <ul class="breadcrumbs">
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="index.html"> Каталог </a>
+          <router-link :to="{ name: 'main' }" class="breadcrumbs__link">
+            Каталог
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="#"> Носки </a>
+          <router-link :to="{ name: '' }" class="breadcrumbs__link">
+            {{ product.category.title }}
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link"> Носки с принтом мороженое </a>
+          <router-link :to="{ name: '' }" class="breadcrumbs__link">
+            {{ product.title }}
+          </router-link>
         </li>
       </ul>
     </div>
@@ -19,33 +28,25 @@
       <div class="item__pics pics">
         <div class="pics__wrapper">
           <img
-            width="570"
-            height="570"
-            src="img/product-square-1.jpg"
-            srcset="img/product-square-1@2x.jpg 2x"
-            alt="Название товара"
+            class="pics__image"
+            :src="currentColorGalleryImage"
+            :alt="product.title"
           />
         </div>
         <ul class="pics__list">
-          <li class="pics__item">
-            <a href="" class="pics__link pics__link--current">
+          <li
+            v-for="color in product.colors"
+            :key="color.id"
+            class="pics__item"
+          >
+            <a href="#" class="pics__link">
               <img
-                width="98"
-                height="98"
-                src="img/product-square-2.jpg"
-                srcset="img/product-square-2@2x.jpg 2x"
-                alt="Название товара"
-              />
-            </a>
-          </li>
-          <li class="pics__item">
-            <a href="" class="pics__link">
-              <img
-                width="98"
-                height="98"
-                src="img/product-square-3.jpg"
-                srcset="img/product-square-3@2x.jpg 2x"
-                alt="Название товара"
+                class="pics__link-image"
+                :src="color.gallery[0].file.url"
+                :alt="product.title"
+                :class="{
+                  'pics__link--current': selectedColorId === color.color.id,
+                }"
               />
             </a>
           </li>
@@ -53,8 +54,8 @@
       </div>
 
       <div class="item__info">
-        <span class="item__code">Артикул: 150030</span>
-        <h2 class="item__title">Смартфон Xiaomi Mi Mix 3 6/128GB</h2>
+        <span class="item__code">Артикул: {{ currentSku }}</span>
+        <h2 class="item__title">{{ product.title }}</h2>
         <div class="item__form">
           <form class="form" action="#" method="POST">
             <div class="item__row item__row--center">
@@ -74,57 +75,30 @@
                 </button>
               </div>
 
-              <b class="item__price"> 18 990 ₽ </b>
+              <b class="item__price"> {{ product.price | numberFormat }} ₽ </b>
             </div>
 
             <div class="item__row">
               <fieldset class="form__block">
                 <legend class="form__legend">Цвет</legend>
                 <ul class="colors colors--black">
-                  <li class="colors__item">
+                  <li
+                    v-for="color in product.colors"
+                    :key="color.id"
+                    class="colors__item"
+                  >
                     <label class="colors__label">
                       <input
                         class="colors__radio sr-only"
                         type="radio"
-                        name="color-item"
-                        value="blue"
-                        checked=""
+                        :value="color.color.id"
+                        v-model="selectedColorId"
                       />
                       <span
                         class="colors__value"
-                        style="background-color: #73b6ea"
-                      >
-                      </span>
+                        :style="{ 'background-color': color.color.code }"
+                      ></span>
                     </label>
-                  </li>
-                  <li class="colors__item">
-                    <label class="colors__label">
-                      <input
-                        class="colors__radio sr-only"
-                        type="radio"
-                        name="color-item"
-                        value="yellow"
-                      />
-                      <span
-                        class="colors__value"
-                        style="background-color: #ffbe15"
-                      >
-                      </span>
-                    </label>
-                  </li>
-                  <li class="colors__item">
-                    <label class="colors__label">
-                      <input
-                        class="colors__radio sr-only"
-                        type="radio"
-                        name="color-item"
-                        value="gray" />
-                      <span
-                        class="colors__value"
-                        style="background-color: #939393"
-                      >
-                      </span
-                    ></label>
                   </li>
                 </ul>
               </fieldset>
@@ -134,10 +108,18 @@
                 <label
                   class="form__label form__label--small form__label--select"
                 >
-                  <select class="form__select" type="text" name="category">
-                    <option value="value1">37-39</option>
-                    <option value="value2">40-42</option>
-                    <option value="value3">42-50</option>
+                  <select
+                    class="form__select"
+                    type="text"
+                    v-model="selectedSizeId"
+                  >
+                    <option
+                      v-for="size in product.sizes"
+                      :key="size.id"
+                      :value="size.id"
+                    >
+                      {{ size.title }}
+                    </option>
                   </select>
                 </label>
               </fieldset>
@@ -160,32 +142,86 @@
           </li>
         </ul>
 
-        <div class="item__content">
-          <h3>Доставка:</h3>
-
-          <p>
-            1. Курьерская доставка по Москве и Московской области – 290 ₽<br />
-            2.Самовывоз из магазина. Список и адреса магазинов Вы можете
-            посмотреть здесь<br />
-          </p>
-
-          <h3>Возврат:</h3>
-
-          <p>
-            Любой возврат должен быть осуществлен в течение Возвраты в магазине
-            БЕСПЛАТНО.<br />
-            Вы можете вернуть товары в любой магазин. Магазин должен быть
-            расположен в стране, в которой Вы осуществили покупку. БЕСПЛАТНЫЙ
-            возврат в Пункт выдачи заказов.<br />
-            Для того чтобы вернуть товар в одном из наших Пунктов выдачи
-            заказов, позвоните по телефону 8 800 600 90 09<br />
-          </p>
-        </div>
+        <div class="item__content"></div>
       </div>
     </section>
   </main>
 </template>
 
 <script>
-export default {};
+// eslint-disable-next-line
+import axios from 'axios';
+import numberFormat from '@/helpers/numberFormat';
+import { API_BASE_URL } from '@/config';
+import CustomLoader from '@/components/CustomLoader.vue';
+
+export default {
+  components: { CustomLoader },
+  data() {
+    return {
+      product: null,
+      loading: false,
+      error: null,
+
+      currentSku: null,
+
+      selectedColorId: null,
+      selectedSizeId: null,
+      selectedQuantity: 1,
+    };
+  },
+  filters: {
+    numberFormat,
+  },
+  methods: {
+    // prettier-ignore
+    async loadProduct() {
+      try {
+        this.loading = true;
+        const productId = this.$route.params.id;
+        const { data } = await axios.get(
+          `${API_BASE_URL}/api/products/${productId}`,
+        );
+        this.product = data;
+
+        this.currentSku = this.product.colors.id;
+        if (this.product.colors && this.product.colors.length > 0) {
+          this.selectedColorId = this.product.colors[0].color.id;
+        }
+        if (this.product.sizes && this.product.sizes.length > 0) {
+          this.selectedSizeId = this.product.sizes[0].id;
+        }
+      } catch (error) {
+        console.error('Произошла ошибка при загрузке продукта:', error);
+        this.error = 'Произошла ошибка при загрузке продукта';
+      } finally {
+        this.loading = false;
+      }
+    },
+    // prettier-ignore
+    getSelectedColor() {
+      return this.product.colors.find(
+        (color) => color.color.id === this.selectedColorId,
+      );
+    },
+    handleColorChange() {
+      const selectedColor = this.getSelectedColor();
+      if (selectedColor) {
+        this.currentSku = selectedColor.id;
+      }
+    },
+  },
+  computed: {
+    currentColorGalleryImage() {
+      const selectedColor = this.getSelectedColor();
+      return selectedColor ? selectedColor.gallery[0].file.url : '';
+    },
+  },
+  watch: {
+    selectedColorId: 'handleColorChange',
+  },
+  created() {
+    this.loadProduct();
+  },
+};
 </script>
