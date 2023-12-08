741213363c998faffa8765f63d9904728f7ea03c
Masha Moreva <mashamiskova@yandex.ru>
2023-12-08 16:24:17 +0300

    refactor-add-cart-indicator

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,16 +14,15 @@
 
       <a class="header__tel" href="tel:8 800 600 90 09"> 8 800 600 90 09 </a>
 
-      <a class="header__cart" href="cart.html" aria-label="Корзина с товарами">
-        <svg width="19" height="24">
-          <use xlink:href="#icon-cart"></use>
-        </svg>
-        <span class="header__count" aria-label="Количество товаров">3</span>
-      </a>
+      <CartIndicator />
     </div>
   </header>
 </template>
 
 <script>
-export default {};
+import CartIndicator from './CartIndicator.vue';
+
+export default {
+  components: { CartIndicator },
+};
 </script>
