39ca9739144ed4a6f519ac0471fe417e0fdc9f7d
Masha Moreva <mashamiskova@yandex.ru>
2023-12-08 16:21:05 +0300

    created-new-cart-indicator-component

---
+++ src/components/CartIndicator.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link
+    :to="{ name: 'cart' }"
+    class="header__cart"
+    aria-label="Корзина с товарами"
+  >
+    <svg width="19" height="24">
+      <use xlink:href="#icon-cart"></use>
+    </svg>
+    <span class="header__count" aria-label="Количество товаров">{{
+      $store.state.cartProducts.length
+    }}</span>
+  </router-link>
+</template>
+
+<script>
+export default {};
+</script>
