b1aef85dbaf53ea2dc420c721d4c78990d2e06b3
Masha Moreva <mashamiskova@yandex.ru>
2023-12-12 19:52:03 +0300

    create-form-field-mixin

---
+++ src/mixins/formFieldMixin.js
@@ -0,0 +1,16 @@
+import BaseFormField from '@/components/BaseFormField.vue';
+
+export default {
+  components: { BaseFormField },
+  props: ['title', 'error', 'placeholder', 'value'],
+  computed: {
+    dataValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
