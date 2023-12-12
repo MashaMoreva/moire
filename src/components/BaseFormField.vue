403d92c70cc9d411b714dff9bd46c414bf0fc663
Masha Moreva <mashamiskova@yandex.ru>
2023-12-12 19:55:54 +0300

    create-base-form-field

---
+++ src/components/BaseFormField.vue
@@ -0,0 +1,12 @@
+<template>
+  <label class="form__label">
+    <slot />
+    <span class="form__value">{{ title }}</span>
+    <span v-if="error" class="form__error">{{ error }}</span>
+  </label>
+</template>
+<script>
+export default {
+  props: ['title', 'error'],
+};
+</script>
