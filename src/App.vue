d45ed464cfeadca7d2475e94c421c613128597ef
Masha Moreva <mashamiskova@yandex.ru>
2023-12-12 10:22:23 +0300

    add-load-cart-method

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,24 @@
 <script>
 import AppHeader from '@/components/AppHeader.vue';
 import AppFooter from '@/components/AppFooter.vue';
+import { mapActions, mapMutations } from 'vuex';
 
 export default {
   components: {
     AppHeader,
     AppFooter,
+  },
+  created() {
+    const userAccessKey = localStorage.getItem('userAccessKey');
+    if (userAccessKey) {
+      this.updateUserAccessKey(userAccessKey);
+    }
+
+    this.loadCart(); // === this.$store.dispatch('loadCart');
+  },
+  methods: {
+    ...mapActions(['loadCart']),
+    ...mapMutations(['updateUserAccessKey']),
   },
 };
 </script>
