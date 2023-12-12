cb9d53c3d162babfef23b1193b8bff7ce01794ad
Masha Moreva <mashamiskova@yandex.ru>
2023-12-12 19:56:19 +0300

    create-base-form-text-component

---
+++ src/components/BaseFormText.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<template>
+  <BaseFormField :title="title" :error="error">
+    <input
+      class="form__input"
+      :type="type"
+      :placeholder="placeholder"
+      v-model="dataValue"
+    />
+  </BaseFormField>
+</template>
+<script>
+import formFieldMixin from '@/mixins/formFieldMixin';
+
+export default {
+  props: {
+    type: {
+      default: 'text',
+    },
+  },
+  mixins: [formFieldMixin],
+};
+</script>
