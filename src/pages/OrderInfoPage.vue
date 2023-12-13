24bb2d7349be110aeb0dbabceacde960f4373dac
Masha Moreva <mashamiskova@yandex.ru>
2023-12-13 11:17:59 +0300

    refactor-removed-hard-coding

--- src/pages/OrderInfoPage.vue
+++ src/pages/OrderInfoPage.vue
@@ -1,80 +1,109 @@
 <template>
-  <main class="content container">
+  <main v-if="!orderInfo" class="content container">
+    <CustomLoader />
+  </main>
+  <main v-else class="content container">
     <div class="content__top">
       <ul class="breadcrumbs">
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="index.html"> Каталог </a>
+          <router-link :to="{ name: 'main' }" class="breadcrumbs__link">
+            Каталог
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="cart.html"> Корзина </a>
+          <router-link :to="{ name: 'cart' }" class="breadcrumbs__link">
+            Корзина
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link"> Оформление заказа </a>
+          <p class="breadcrumbs__link">Оформление заказа</p>
         </li>
       </ul>
 
-      <h1 class="content__title">Заказ оформлен <span>№ 23621</span></h1>
+      <h1 class="content__title">
+        Заказ оформлен <span>№ {{ orderInfo.id }}</span>
+      </h1>
     </div>
 
     <section class="cart">
       <form class="cart__form form" action="#" method="POST">
         <div class="cart__field">
           <p class="cart__message">
-            Благодарим за&nbsp;выбор нашего магазина. На&nbsp;Вашу почту придет
-            письмо с&nbsp;деталями заказа. Наши менеджеры свяжутся с&nbsp;Вами
-            в&nbsp;течение часа для уточнения деталей доставки.
+            Благодарим за&nbsp;выбор нашего магазина. В ближайшее время наши
+            менеджеры свяжутся с&nbsp;Вами. Оставайтесь на связи и готовьтесь к
+            стильным переменам!
           </p>
 
           <ul class="dictionary">
             <li class="dictionary__item">
               <span class="dictionary__key"> Получатель </span>
               <span class="dictionary__value">
-                Иванова Василиса Алексеевна
+                {{ orderInfo.name }}
               </span>
             </li>
             <li class="dictionary__item">
               <span class="dictionary__key"> Адрес доставки </span>
               <span class="dictionary__value">
-                Москва, ул. Ленина, 21, кв. 33
+                {{ orderInfo.address }}
               </span>
             </li>
             <li class="dictionary__item">
               <span class="dictionary__key"> Телефон </span>
-              <span class="dictionary__value"> 8 800 989 74 84 </span>
+              <span class="dictionary__value"> {{ orderInfo.phone }} </span>
             </li>
             <li class="dictionary__item">
               <span class="dictionary__key"> Email </span>
-              <span class="dictionary__value"> lalala@mail.ru </span>
+              <span class="dictionary__value"> {{ orderInfo.email }} </span>
+            </li>
+            <li class="dictionary__item">
+              <span class="dictionary__key"> Способ доставки </span>
+              <span class="dictionary__value">
+                {{ orderInfo.deliveryType.title }}
+              </span>
             </li>
             <li class="dictionary__item">
               <span class="dictionary__key"> Способ оплаты </span>
-              <span class="dictionary__value"> картой при получении </span>
+              <span class="dictionary__value">
+                {{ orderInfo.paymentType }}
+              </span>
             </li>
           </ul>
         </div>
 
         <div class="cart__block">
           <ul class="cart__orders">
-            <li class="cart__order">
-              <h3>Смартфон Xiaomi Redmi Note 7 Pro 6/128GB</h3>
-              <b>990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-            <li class="cart__order">
-              <h3>Гироскутер Razor Hovertrax 2.0ii</h3>
-              <b>1 990 ₽</b>
-              <span>Артикул: 150030</span>
-            </li>
-            <li class="cart__order">
-              <h3>Электрический дрифт-карт Razor Lil’ Crazy</h3>
-              <b>4 090 ₽</b>
-              <span>Артикул: 150030</span>
+            <li
+              v-for="item in orderInfo.basket.items"
+              :key="item.id"
+              class="cart__order"
+            >
+              <h3>{{ item.product.title }}</h3>
+              <b
+                ><b>x{{ item.quantity }}</b
+                >{{ item.price * item.quantity }} ₽</b
+              >
+              <span>Артикул: {{ item.color.id }}</span>
             </li>
           </ul>
 
           <div class="cart__total">
-            <p>Доставка: <b>бесплатно</b></p>
-            <p>Итого: <b>3</b> товара на сумму <b>4 070 ₽</b></p>
+            <p>
+              Доставка:
+              <b>{{
+                orderInfo.deliveryType.price === '0'
+                  ? 'бесплатно'
+                  : orderInfo.deliveryType.price + ' ₽'
+              }}</b>
+            </p>
+            <p>
+              Товары: <b>{{ orderInfo.basket.items.length }}</b>
+              {{ getCorrectEnding(orderInfo.basket.items.length) }} на сумму
+              <b>{{ orderInfoTotalPrice }} ₽</b>
+            </p>
+            <p>
+              Итого:
+              <b>{{ orderInfo.totalPrice }} ₽</b>
+            </p>
           </div>
         </div>
       </form>
@@ -83,5 +112,31 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from 'vuex';
+import getCorrectEnding from '@/helpers/getCorrectEnding';
+import CustomLoader from '@/components/CustomLoader.vue';
+
+export default {
+  components: { CustomLoader },
+  methods: {
+    getCorrectEnding,
+  },
+  computed: {
+    ...mapGetters({
+      orderInfo: 'orderInfo',
+      orderInfoTotalPrice: 'orderInfoTotalPrice',
+    }),
+  },
+  created() {
+    if (
+      // prettier-ignore
+      this.$store.state.orderInfo
+      && this.$store.state.orderInfo.id === this.$route.params.id
+    ) {
+      return;
+    }
+
+    this.$store.dispatch('loadOrderInfo', this.$route.params.id);
+  },
+};
 </script>
